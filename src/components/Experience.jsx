e4ffe017144bee48be703f53653d9f8e8e50fc8f
vishwdeep2001 <vishwdeepsingharia@gmail.com>
2024-05-06 01:26:47 +0530

    work project

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,13 +7,13 @@ import {experiences} from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant  } from '../utils/motion';
 
-const ExperienceCard = ({experience})=>{
+const ExperienceCard = ({experience})=>(
   <VerticalTimelineElement contentStyle={{background:'#1d1836',color:'#fff'}}
   contentArrowStyle={{borderRight:'7px solid #232631'}}
   date={experience.date}
   iconStyle={{background:experience.iconBg}}
   icon={
-    <div>
+    <div className='flex justify-center items-center w-full h-full'>
       <img
       src={experience.icon}
       alt={experience.company_name}
@@ -23,9 +23,19 @@ const ExperienceCard = ({experience})=>{
   }>
     <div>
       <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+   <p className='text-secondary text-[16px] font-semibold' style={{margin:0}}>{experience.company_name}</p>
     </div>
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point,index)=>(
+        <li
+        key={`experience-point-${index}`}
+        className='text-white-100 text-[14px] pl-1 tracking-wider'>
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
-}
+)
 
 const Experience = () => {
   return (
